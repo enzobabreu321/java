ced0908644cffb90a06b72fbdec257806539c3af
Enzo Boccardo de Abreu <119687301+enzobabreu321@users.noreply.github.com>
2025-08-12 11:55:06 -0300

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -2,5 +2,9 @@ git clone https://github.com/enzobabreu321/Financiamento.git
 jar cvfm java-executable\Financiamento.jar MANIFEST.mf Main.class simpleh\JSimple.class
 del MANIFEST.mf Main.class
 rmdir /s /q simpleh
+mkdir java-executable
+echo @echo off > java-executable/start.bat
+echo echo java -jar Financiamento.jar >> java-executable/start.bat
+echo pause >> java-executable/start.bat
 java -jar ./java-executable/Financiamento.jar
 pause
